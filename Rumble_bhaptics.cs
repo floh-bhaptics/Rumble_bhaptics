a6febf7d6ea2d2b82fe76cbf3ad4ad914ff74200
floh-bhaptics <floh@fahrenbergers.de>
2022-12-12 21:53:09 +0100

    Add project files.

---
+++ Rumble_bhaptics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+using RUMBLE.MoveSystem;
+using RUMBLE.Players;
+
+namespace Rumble_bhaptics
+{
+    public class Rumble_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(CombatManager), "RegisterPlayerHitEvent", new Type[] { typeof(Player), typeof(Structure) })]
+        public class bhaptics_PlayerHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(CombatManager __instance, Structure structure, Player player)
+            {
+                if (player.Controller.controllerType != ControllerType.Local) return;
+            }
+        }
+
+        [HarmonyPatch(typeof(CombatManager), "RegisterPlayerHitFromBeneathEvent", new Type[] { typeof(Player), typeof(Structure) })]
+        public class bhaptics_PlayerHitFromBeneath
+        {
+            [HarmonyPostfix]
+            public static void Postfix(CombatManager __instance, Structure structure, Player player)
+            {
+                if (player.Controller.controllerType != ControllerType.Local) return;
+            }
+        }
+
+        [HarmonyPatch(typeof(RUMBLE.Players.Subsystems.PlayerHealth), "UpdateLocalHealthbarPercentage", new Type[] { typeof(float), typeof(bool) })]
+        public class bhaptics_PlayerUpdateHealth
+        {
+            [HarmonyPostfix]
+            public static void Postfix(RUMBLE.Players.Subsystems.PlayerHealth __instance, float percentage)
+            {
+                if (percentage <= 0.25f) tactsuitVr.StartHeartBeat();
+                else tactsuitVr.StopHeartBeat();
+            }
+        }
+
+
+    }
+}
